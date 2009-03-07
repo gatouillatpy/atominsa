8df0571a82bd9f8e527e659d67205cb125ee9fbb
gatouillatpy <gatouillatpy>
2009-03-07 12:31:50 +0000

    correction du bug de deconnexion du master server lors de la selection d'un joueur
    git-svn-id: https://svn.code.sf.net/p/atominsa/code@208 a550dbb0-cc32-0410-9e1e-cdff82656a7a

--- master/AtominsaServer.bat
+++ master/AtominsaServer.bat
@@ -1,7 +1,9 @@
-@set path=%path%;"C:\Program Files\Java\jdk1.6.0_12\bin"
-@cd src
-@javac *.java
-@move /y *.class ..\bin
-@cd ..\bin
-@java AtominsaMaster
-@cd..
+@echo off
+set path=%path%;"C:\Program Files\Java\jdk1.6.0_12\bin"
+mkdir bin
+cd src
+javac *.java
+move /y *.class ..\bin
+cd ..\bin
+java -Xbootclasspath/p:..\lib\mysql-connector-java-5.1.7-bin.jar AtominsaMaster
+cd..
