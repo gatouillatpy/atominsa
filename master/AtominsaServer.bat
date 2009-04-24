71ea080ed34c0e1d2008154a53776c9274fa315e
gatouillatpy <gatouillatpy>
2009-04-24 16:46:57 +0000

    git-svn-id: https://svn.code.sf.net/p/atominsa/code@241 a550dbb0-cc32-0410-9e1e-cdff82656a7a

--- master/AtominsaServer.bat
+++ master/AtominsaServer.bat
@@ -1,9 +1,9 @@
 @echo off
-set path=%path%;"C:\Program Files\Java\jdk1.6.0_05\bin"
+set path=%path%;"C:\Program Files\Java\jdk1.6.0_12\bin"
 mkdir bin
 cd src
 javac *.java
 move /y *.class ..\bin
 cd ..\bin
-java -Xbootclasspath/p:..\lib\mysql-connector-java-5.1.7-bin.jar AtominsaMaster
+java -Xbootclasspath/p:..\lib\dbw.jar AtominsaMaster
 cd..
